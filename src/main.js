79995754d4a11a9c43440728b21fd3e6869af1bd
huseyinkural <huseyinkural35@gmail.com>
2020-03-20 22:06:01 +0300

    vue-router has implemented

--- src/main.js
+++ src/main.js
@@ -1,7 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import VueRouter from 'vue-router';
+import Vuex from 'vuex';
+
+import Home from './components/Home.vue';
+
+Vue.use(VueRouter);
+Vue.use(Vuex);
+
+const router = new VueRouter({
+
+  routes: [
+    {
+      path: '/',
+      component: Home
+    }
+  ]
+
+});
+
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
